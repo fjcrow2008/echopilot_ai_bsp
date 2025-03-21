b0cd1dec158c24600b6652443fcc67957f8c8902
fcrow <fcrow@idealaero.com>
2025-03-21 14:12:57 -0400

    Added EchoPilotAI-SimOnHardWare board configuration for ArduPilot. Cleanup hwdef and parameters.   Add SimOnHardWare bootloader binary.  Update installation script. Removed HAL_GPIO_LED_ON because setting it to the default is not supported in ArduPilot 4.7.0. Corrected PD0 and PD1 GPIO definitions on UART4. Removed UART4 from SERIAL_ORDER. Fixed issues with ArduPilot 4.6.0 and added parm file for SITL build.

--- install_ardupilot.sh
+++ install_ardupilot.sh
@@ -23,7 +23,10 @@ echo "Copying hwdef files..."
 
 # copy files
 cp ardupilot/libraries/AP_HAL_ChibiOS/hwdef/EchoPilotAI -r $INSTALL_PATH/libraries/AP_HAL_ChibiOS/hwdef/.
+cp ardupilot/libraries/AP_HAL_ChibiOS/hwdef/EchoPilotAI-SimOnHardWare -r $INSTALL_PATH/libraries/AP_HAL_ChibiOS/hwdef/.
+cp ardupilot/libraries/AP_HAL_ChibiOS/hwdef/EchoPilotAI-SimOnHardWare/TM-Combined-Parameters.parm $INSTALL_PATH
 cp ardupilot/Tools/bootloaders/EchoPilotAI_bl.bin $INSTALL_PATH/Tools/bootloaders/.
+cp ardupilot/Tools/bootloaders/EchoPilotAI-SimOnHardWare_bl.bin $INSTALL_PATH/Tools/bootloaders/.
 
 echo "Complete."
 echo "You may now build ardupilot (e.g. copter) using:"
